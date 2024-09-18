0d1adab3ac4acade7b26688d19f5cfbe7b3f5809
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-09-17 21:34:13 -0400

    Checkpoint where original functionality was recreated, before adding front end

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # rust-rocket-web-server-rewrite
 
-This is a rewrite from scratch of the rust-rocket-web-server functionality, in order to create it from memory and without looking up any of the reference material that was used to learn and make the original.
+This is a rewrite from scratch of the rust-rocket-web-server functionality, in order to create it from memory and without looking up any of the reference material from the video lesson that was used to learn and make the original.
 
 A simple front end might also potentially be added to this version as a way of extending it.
+
+
+
+The only material looked at besides memory is the Rocket documentation itself in order to set up the initial boilerplate, and then if I got stuck at any point I would check the original material while noting which parts those were.
+
+
+List of things I had to look up in the source material due to getting stuck when using only memory recall:
+
+1)  I had to look at the original code for this one line, as it wasn't working from writing it like this from memory
+    // let results: rusqlite::Result<Vec<ToDoItem>> = rows.collect();
+
+    For lines 74..87, I had to make brief glances at the source material to refresh myself on how the results were being collected and turned into the final output.
+
+
+2)  Once the fetch_all_todo_items endpoint was completed, trying to access it showed that there was an error in the SQL statement portion of the code, so I reviewed the source material to check for errors in the SQL statements.  I saw that I was missing details like 'primary key' and 'not null' in the creation statements, but correcting these didn't solve the problem yet.
+
+    I modified my error message to include the SQL error message from the Result instead of throwing it away, and it said there was no such table as todo_items
+
+    I traced the problem back by reading my a println! of the result of creating the table, until I saw that it was failing to create it because it said "create table it not exists" instead of "create table if not exists"
+
+
+
+3)  Mention of things I had to ask github copilot to remind me of:
+
+    I knew that in cargo.toml, I had to add a feature to rusqlite to make it work properly, but forgot what it was called.  It provided me with a reminder:
+        - rusqlite = { version = "0.32.1", features = ["bundled"] }
+
+
+Besides these things, I was able to produce the functionality of the original Rocker server example without having to look at anything to remind myself of how.
+
+Now that this is done, the next step will be to investigate ways to add a front end to it so that the todo list can be fetched and rendered as something like an unordered list, with each item having a delete button to remove it, and a button to add new items to the list.
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,140 @@
+#[macro_use]
+extern crate rocket;
 
-fn main() {
+use core::num;
+use std::result;
 
-    
+use rocket::{http::Status, serde::{json::Json, Deserialize, Serialize}, State};
+
+#[launch]
+fn launch_rocket() -> _ {
+    //create a sqlite database and make sure a 'todo_list' table exists on it
+    {
+
+        println!("Initializing server");
+
+        let db_connection = rusqlite::Connection::open("data.sqlite")
+            .expect("Database connection failed.  Terminating program.");
+
+        let result = db_connection.execute(
+            "CREATE TABLE if not exists todo_items
+                (
+                    id integer primary key,
+                    item varchar(255) not null
+                );",
+            (),
+        );
+
+        println!("{:?}", result);
+    } //database connection is made in this inner scope so that it is dropped after creating it.  Will reconnect on each request for this application.
+
+    rocket::build().mount(
+        "/",
+        routes![index, fetch_todo_items, add_todo_item, delete_todo_item],
+    )
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to the rewritten rocket server!"
+}
+
+#[derive(Serialize)]
+struct ToDoList {
+    items: Vec<ToDoItem>,
+}
+
+#[derive(Serialize)]
+struct ToDoItem {
+    id: i64,
+    item: String,
+}
+
+#[derive(Serialize)]
+struct StatusMessage {
+    message: String,
+}
+
+#[get("/todo")]
+fn fetch_todo_items() -> Result<Json<ToDoList>, String> {
+
+
+    let db_connection = match rusqlite::Connection::open("data.sqlite") {
+        Ok(connection) => connection,
+        Err(_) => return Err(String::from("Unable to connect to database"))
+    };
+
+    let mut statement = match db_connection.prepare("select * from todo_items;") {
+        Ok(statement) => statement,
+        Err(message) => return Err(String::from("Error creating SQL statement") + &format!("{}", message))
+    };
+
+    let results = statement.query_map((), |row| {
+        Ok(
+            ToDoItem {
+                id: row.get(0)?,
+                item: row.get(1)?
+            }
+        )
+    });
+
+
+    //I had to look at the original code for this one line, as it wasn't working from writing it like this from memory
+    // let results: rusqlite::Result<Vec<ToDoItem>> = rows.collect();
+
+    match results {
+        Ok(rows) => {
+
+            let collection: rusqlite::Result<Vec<ToDoItem>> = rows.collect();
+
+            match collection {
+                Ok(items) => Ok(Json(ToDoList{ items })),
+                Err(_) => Err(String::from("Error gathering rows into a vector of ToDoItems"))
+            }
+        }
+        Err(_) => Err(String::from("Unable to parse rows from results"))
+        
+    }
+
+}
+
+
+#[post("/todo", format = "json", data = "<item>")]
+fn add_todo_item(item: Json<String>) -> Result<Json<StatusMessage>, String> {
+    let db_connection = match rusqlite::Connection::open("data.sqlite") {
+        Ok(connection) => connection,
+        Err(_) => return Err(String::from("Error connecting to database"))
+    };
+
+    let mut statement = match db_connection.prepare("insert into todo_items values (null, $1);") {
+        Ok(statement) => statement,
+        Err(_) => return Err(String::from("Failed to prepare SQL statement"))
+    };
+
+    let result = statement.execute(&[&item.0]);
+
+    match result {
+        Ok(number_rows) => Ok(Json(StatusMessage { message: format!("{} rows inserted", number_rows)})),
+        Err(_) => return Err(String::from("Error inserting values"))
+    }
+}
+
+#[delete("/todo/<id>")]
+fn delete_todo_item(id: i64) -> Result<Json<StatusMessage>, String> {
+    let db_connection = match rusqlite::Connection::open("data.sqlite") {
+        Ok(connection) => connection,
+        Err(_) => return Err(String::from("Error connecting to database"))
+    };
+
+    let mut statement = match db_connection.prepare("delete from todo_items where id = $1;") {
+        Ok(statement) => statement,
+        Err(_) => return Err(String::from("Error preparing statement"))
+    };
+
+    let result = statement.execute(&[&id]);
+
+    match result {
+        Ok(number_rows) => Ok(Json(StatusMessage{ message: format!("{} rows deleted", number_rows)})),
+        Err(_) => return Err(String::from("Error deleting rows"))
+    }
 }
